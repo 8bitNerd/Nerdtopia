f1560808cd561e38504855db7e164c68b6a9b3c3
8bitNerd <nerd@nerdtopia.xyz>
2016-02-11 23:40:03 +0000

    Added website files

---
+++ website/login/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login | Nerdtopia</title>
+        <link href="../css/bootstrap.min.css" rel="stylesheet" />
+        <link href="../css/materialize.min.css" rel="stylesheet" />
+        <link href="../css/custom.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div style="margin-top: 25px;">
+            <div class="col-lg-4 col-md-3 col-sm-2 col-xs-1"></div>
+            <div class="col-lg-4 col-md-6 col-sm-8 col-xs-10">
+                <div align="center">
+                    <img src="../img/base_icon.png" style="height: 96px;"/>
+                    <h2>Nerdtopia</h2>
+                    <p style="font-size: 120%; margin-top: -15px;">The perfect place for nerds</p>
+                </div>
+                <div class="row" style="margin-top: 40px;">
+                    <div class="card-panel white">
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="email" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top: -20px;">
+                            <div class="input-field col s12">
+                                <input id="password" type="password" class="validate">
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+                        <div class="row" align="right" style="margin-right: 0px; margin-bottom: 5px;">
+                            <a class="waves-effect waves-light btn">Sign in</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-3 col-sm-2 col-xs-1"></div>
+        </div>
+        <script src="../js/jquery-1.12.0.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/materialize.min.js"></script>
+    </body>
+</html>
